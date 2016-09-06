1a9d8f3a65edc144421e9034ac8fa9109a109c58
Liam Considine <liam@Liam-Considines-MacBook-Air.local>
2016-09-06 14:00:53 -0400

    Added another logger

--- exception.rb
+++ exception.rb
@@ -1,5 +1,14 @@
 require 'logger'
+
+logfile = Logger.new('test.log')
 log = Logger.new(STDOUT)
+
+logfile.debug "debugging info"
+logfile.info "general logs"
+logfile.warn "oh my…this isn't good"
+logfile.error "boom!"
+logfile.fatal "oh crap…"
+
 begin
   [2, 1].each do |n|
     if n <= 1
@@ -10,3 +19,6 @@ begin
   rescue Exception
     log.info "Something in the array was greater than 1"
   end
+
+logfile.close
+log.close
