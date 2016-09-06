9025c59f44c6870d077842d3a444192adeb09544
Liam Considine <liam@Liam-Considines-MacBook-Air.local>
2016-09-06 15:50:14 -0400

    I understand rspec syntax now better

--- rspec_play.rb
+++ rspec_play.rb
@@ -1,5 +1,5 @@
+Dir["/Users/liam/dev/RubyPlayground/lib/*.rb"].each {|file| require file }
 require 'rspec'
-#require 'spec_helper'
 
 $count = 0
 describe "test" do
@@ -28,5 +28,22 @@ describe "test 2" do
   let(:count) {$count}
   it "treats variables locally in describe blocks" do
     expect(count).to eq(3)
+  end
+end
+
+describe "testing Adtegrity module" do
+  let(:friend) {Adtegrity::Friend.new}
+
+  it "initializes correctly" do
+    expect(friend.name).to eq("New Employee")
+  end
+
+  it "counts friend list length" do
+    friend.add(["Liam", "George", "Patty"])
+    expect(friend.count_friends).to eq(3)
+  end
+
+  it "fails when friends add is not called" do
+    expect(friend.count_friends).to eq(3)
   end
 end
