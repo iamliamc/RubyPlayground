367e1ca2c61afd56fc2c29fcf55014922a85369f
Liam Considine <liam.considine@adtegrity.com>
2016-10-25 16:15:45 -0400

    End of refactoring example 1, move frequent renter points and charge to price classes

--- RefactoringRuby/movie.rb
+++ RefactoringRuby/movie.rb
@@ -37,7 +37,7 @@ class Movie
   end
 
   def frequent_renter_points(days_rented)
-    (price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2: 1
+    @price.frequent_renter_points(days_rented)
   end
 
   def charge(days_rented)
@@ -46,7 +46,14 @@ class Movie
 
 end
 
+module DefaultPrice
+  def frequent_renter_points(days_rented)
+    1
+  end
+end
+
 class RegularPrice
+  include DefaultPrice
   def charge(days_rented)
     result = 2
     result += (days_rented - 2) * 1.5 if days_rented > 2
@@ -58,9 +65,14 @@ class NewReleasePrice
   def charge(days_rented)
     days_rented * 3
   end
+
+  def frequent_renter_points(days_rented)
+    days_rented > 1 ? 2 : 1
+  end
 end
 
 class ChildrensPrice
+  include DefaultPrice
   def charge(days_rented)
     result = 1.5
     result += (days_rented - 3) * 1.5 if days_rented > 3
