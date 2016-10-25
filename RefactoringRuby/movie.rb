5add2fea172bd471414e564a1a3629c7e0ce85ed
Liam Considine <liam.considine@adtegrity.com>
2016-10-25 16:05:49 -0400

    Movie inherits from price objects

--- RefactoringRuby/movie.rb
+++ RefactoringRuby/movie.rb
@@ -3,11 +3,23 @@ class Movie
   REGULAR = 0
   NEW_RELEASE = 1
   CHILDRENS = 2
-  attr_reader :title
-  attr_accessor :price_code
+  attr_reader :price_code, :title
 
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
+  def price_code=(value)
+    @price_code = value
+    @price =
+      case price_code
+      when REGULAR
+        RegularPrice.new
+      when NEW_RELEASE
+        NewReleasePrice.new
+      when CHILDRENS
+        ChildrensPrice.new
+    end
+  end
+
+  def initialize(title, the_price_code)
+    @title, self.price_code = title, the_price_code
   end
 
   def charge(days_rented)
@@ -15,13 +27,11 @@ class Movie
     # determine amounts for each line
     case price_code
     when REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
+      return charge
     when NEW_RELEASE
-      result += days_rented * 3
+      return charge
     when CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
+      return charge
     end
     result
   end
@@ -30,4 +40,30 @@ class Movie
     (price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2: 1
   end
 
+  def charge(days_rented)
+    @price.charge(days_rented)
+  end
+
+end
+
+class RegularPrice
+  def charge(days_rented)
+    result = 2
+    result += (days_rented - 2) * 1.5 if days_rented > 2
+    result
+  end
+end
+
+class NewReleasePrice
+  def charge(days_rented)
+    days_rented * 3
+  end
+end
+
+class ChildrensPrice
+  def charge(days_rented)
+    result = 1.5
+    result += (days_rented - 3) * 1.5 if days_rented > 3
+    result
+  end
 end
