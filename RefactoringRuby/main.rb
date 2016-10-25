89bf9b5ff8c627cfc76911d9867af056f9206ac6
Liam Considine <liam.considine@adtegrity.com>
2016-10-25 14:49:50 -0400

    Starting point for refactoring ruby

---
+++ RefactoringRuby/main.rb
@@ -0,0 +1,16 @@
+require './customer'
+require './movie'
+require './rental'
+
+m = Movie.new("Happy Gilmore", 0)
+m2 = Movie.new("Happy Gilmore 2", 1)
+
+r = Rental.new(m, 2)
+r2 = Rental.new(m2, 5)
+
+c = Customer.new("Liam")
+
+c.add_rental(r)
+c.add_rental(r2)
+
+c.statement
